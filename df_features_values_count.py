c88457b16d7e37f1d671fd6fe95b3cb435e5ea5e
Pedro A. Moreno-Sanchez <pedromorenosanchec@gmail.com>
2020-01-16 07:39:10 +0100

    Update df_features_values_count.py

--- df_features_values_count.py
+++ df_features_values_count.py
@@ -1,6 +1,6 @@
 ## Function that apply the value_counts() to every features of the dataset
 def df_values(df):
     for i in range(0, len(df.columns)):
-        print("*****start of feature ", i, "*************************")
+        print("*****start of feature ", df.columns[i], "*************************")
         print (df.iloc[:,i].value_counts())
-        print ("*****end of feature ", i, "************************** \n")
+        print ("*****end of feature ", df.columns[i], "************************** \n")
