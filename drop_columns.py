fb29b1b76ef2fbafd8b511043c10552cefcdc6c7
Pedro A. Moreno-Sanchez <pedromorenosanchec@gmail.com>
2020-01-21 07:47:34 +0100

    Update drop_columns.py

--- drop_columns.py
+++ drop_columns.py
@@ -3,3 +3,6 @@ df_source=df_source.drop(df_source.columns[i],axis=1)
 
 #Line to delete columns by its name
 df_source=df_source.drop('column_name',axis=1)
+
+#Afterwards  the index must be reset
+df_source.reset_index(drop=True, inplace=True)
