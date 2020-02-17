3895593a435a33ad8ae2a6a19a6bb0494ff3d73c
Pedro A. Moreno-Sanchez <pedromorenosanchec@gmail.com>
2020-02-17 19:28:30 +0200

    Update nonnull_values_percentajes_by_feature.py

--- nonnull_values_percentajes_by_feature.py
+++ nonnull_values_percentajes_by_feature.py
@@ -1,2 +1,5 @@
 ##This line is a interpretation of the info() altough it give us the percentaje of non-null values per feaure in a dataframe df_source
 (df_source.count()/len(df_source))*100
+
+#The following line counts how many null values has the data set per feature
+df_source.isnull().sum()
